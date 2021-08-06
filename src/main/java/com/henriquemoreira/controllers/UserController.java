ac563dcccffb1eb24657b28c0e13e99cf2469175
Henrique Moreira <henriquemoreiraa@gmail.com>
2021-08-06 17:28:48 -0300

    corrige erro controller

--- src/main/java/com/henriquemoreira/controllers/UserController.java
+++ src/main/java/com/henriquemoreira/controllers/UserController.java
@@ -4,17 +4,17 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.henriquemoreira.entities.User;
 import com.henriquemoreira.services.UserService;
 
-@Controller
+@RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
@@ -27,12 +27,12 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public User newProduct(@RequestBody User entity) {
+	public User newUser(@RequestBody User entity) {
 		return service.newUser(entity);
 	}
 	
 	@GetMapping("/{name}")
-	public User findProductById(@PathVariable String name) {
+	public User findUserByName(@PathVariable String name) {
 		return service.findUserByName(name);
 	}
 }
